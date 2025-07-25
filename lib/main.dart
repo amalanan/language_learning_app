ee06a1dedfbca02b1f823f09759991030d5227e6
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 20:38:27 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:language_learning_app/screens/home_page.dart';
+
+void main() {
+  runApp(const TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+        home: HomePage());
+  }
+}
